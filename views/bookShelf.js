9d507f14e7f824b5b52d21f535101be54f14e1a4
yryskul <mail.iriskul@gmail.com>
2020-09-10 23:33:07 +0300

    english is completed

--- views/bookShelf.js
+++ views/bookShelf.js
@@ -10,7 +10,7 @@ function getBookShelfView(selected) {
       width: "160",
       type: "tiles",
     },
-    template: "<img src='#img#' height='90%' width='100%'><div>#title#</div>",
+    template: "<img src='#img#' height='90%' width='100%'><div style='text-align:center;'>#title#</div>",
     data: getBooks(selected),
     on: {
       onSelectChange: () => {
@@ -57,7 +57,7 @@ function getEnglishBooks() {
     },
     {
       id: 3,
-      title: "Onbeşinci Şua",
+      title: "Elhüccetüz Zehra",
       img: "img/books_cover/rays.jpg",
       sections: getSections("ing15sua/ing15sua", 8),
       content: "res/books/ing15sua/fihrist.txt"
@@ -72,20 +72,20 @@ function getEnglishBooks() {
     {
       id: 6,
       title: "Yirmiüçüncü Söz",
-      img: "img/books_cover/words.jpg",
+      img: "img/books_cover/beliefandman2.jpg",
       sections: getSections("ing23soz/ing23soz", 7),
       content: "res/books/ing23soz/fihrist.txt"
     },
     {
       id: 7,
-      title: "Yirmibeşinci Söz",
+      title: "Mucizat-ı Kuraniye",
       img: "img/books_cover/ing25soz.jpg",
       sections: getSections("ing25soz/ing25soz", 10),
       content: "res/books/ing25soz/fihrist.txt"
     },
     {
       id: 8,
-      title: "Yirmialtıncı Mektub",
+      title: "Şeytanla Münazara",
       img: "img/books_cover/letters.jpg",
       sections: getSections("ing26mektub/ing26mektub", 1),
       content: "res/books/ing26mektub/fihrist.txt"
@@ -98,14 +98,14 @@ function getEnglishBooks() {
     },
     {
       id: 10,
-      title: "Otuzuncu Söz",
+      title: "Ene ve Zerre Risaleleri",
       img: "img/books_cover/ing30soz.jpg",
       sections: getSections("ing30soz/ing30soz", 3),
       content: "res/books/ing30soz/fihrist.txt"
     },
     {
       id: 11,
-      title: "Otuz Birinci Söz",
+      title: "Miraç ve Şakk-ı Kamer Risalesi",
       img: "img/books_cover/words.jpg",
       sections: getSections("ing31soz/ing31soz", 4),
       content: "res/books/ing31soz/fihrist.txt"
@@ -124,71 +124,106 @@ function getEnglishBooks() {
       content: "res/books/ing33soz/fihrist.txt"
     },
     {
-      id: 5,
-      title: "ssss",
-      img: "img/books_cover/words.jpg",
-      sections: getSections("ing22soz/ing22soz", 4),
-      content: "res/books/ing22soz/fihrist.txt"
+      id: 14,
+      title: "Ayet-ül Kübra",
+      img: "img/books_cover/supremesign.jpg",
+      sections: getSections("ingayetulkubra/ingayetulkubra", 8),
+      content: "res/books/ingayetulkubra/fihrist.txt"
     }, {
-      id: 5,
-      title: "ssss",
-      img: "img/books_cover/words.jpg",
-      sections: getSections("ing22soz/ing22soz", 4),
-      content: "res/books/ing22soz/fihrist.txt"
+      id: 15,
+      title: "Gençlik Rehberi",
+      img: "img/books_cover/guideforyouth.jpg",
+      sections: getSections("inggenclik/inggenclik", 17),
+      content: "res/books/inggenclik/fihrist.txt"
     },
     {
-      id: 5,
-      title: "ssss",
-      img: "img/books_cover/words.jpg",
-      sections: getSections("ing22soz/ing22soz", 4),
-      content: "res/books/ing22soz/fihrist.txt"
+      id: 16,
+      title: "Haşir Risalesi",
+      img: "img/books_cover/inghasir.jpg",
+      sections: getSections("inghasir/inghasir", 21),
+      content: "res/books/inghasir/fihrist.txt"
     },
     {
-      id: 5,
-      title: "ssss",
-      img: "img/books_cover/words.jpg",
-      sections: getSections("ing22soz/ing22soz", 4),
-      content: "res/books/ing22soz/fihrist.txt"
+      id: 17,
+      title: "Hastalar Risalesi",
+      img: "img/books_cover/inghastalar.jpg",
+      sections: getSections("inghastalar/inghastalar", 8),
+      content: "res/books/inghastalar/fihrist.txt"
     }, {
-      id: 5,
-      title: "ssss",
-      img: "img/books_cover/words.jpg",
-      sections: getSections("ing22soz/ing22soz", 4),
-      content: "res/books/ing22soz/fihrist.txt"
+      id: 18,
+      title: "İhlas Risaleleri",
+      img: "img/books_cover/ikhlas.jpg",
+      sections: getSections("ingihlas/ingihlas", 4),
+      content: "res/books/ingihlas/fihrist.txt"
     },
     {
-      id: 5,
-      title: "ssss",
-      img: "img/books_cover/words.jpg",
-      sections: getSections("ing22soz/ing22soz", 4),
-      content: "res/books/ing22soz/fihrist.txt"
+      id: 19,
+      title: "Küçük Sözler",
+      img: "img/books_cover/ingkucuksozler.jpg",
+      sections: getSections("ingkucuksozler/ingkucuksozler", 11),
+      content: "res/books/ingkucuksozler/fihrist.txt"
     },
     {
-      id: 5,
-      title: "ssss",
-      img: "img/books_cover/words.jpg",
-      sections: getSections("ing22soz/ing22soz", 4),
-      content: "res/books/ing22soz/fihrist.txt"
+      id: 20,
+      title: "Lemalar",
+      img: "img/books_cover/flashes.jpg",
+      sections: getSections("inglemalar/inglemalar", 46),
+      content: "res/books/inglemalar/fihrist.txt"
     }, {
-      id: 5,
-      title: "ssss",
-      img: "img/books_cover/words.jpg",
-      sections: getSections("ing22soz/ing22soz", 4),
-      content: "res/books/ing22soz/fihrist.txt"
+      id: 21,
+      title: "Mektubat",
+      img: "img/books_cover/letters.jpg",
+      sections: getSections("ingmektubat/ingmektubat", 55),
+      content: "res/books/ingmektubat/fihrist.txt"
     },
     {
-      id: 5,
-      title: "ssss",
-      img: "img/books_cover/words.jpg",
-      sections: getSections("ing22soz/ing22soz", 4),
-      content: "res/books/ing22soz/fihrist.txt"
+      id: 22,
+      title: "Meyve Risalesi",
+      img: "img/books_cover/ingmeyve.jpg",
+      sections: getSections("ingmeyve/ingmeyve", 9),
+      content: "res/books/ingmeyve/fihrist.txt"
     },
     {
-      id: 5,
-      title: "ssss",
+      id: 23,
+      title: "Münacat Risalesi (3.Şua)",
+      img: "img/books_cover/rays.jpg",
+      sections: getSections("ingmunacat/ingmunacat", 2),
+      content: "res/books/ingmunacat/fihrist.txt"
+    },
+    {
+      id: 24,
+      title: "Ramazan-İktisat-Şükür Risalesi",
+      img: "img/books_cover/ramadan.jpg",
+      sections: getSections("ingramazan/ingramazan", 3),
+      content: "res/books/ingramazan/fihrist.txt"
+    },
+    {
+      id: 25,
+      title: "Sözler",
       img: "img/books_cover/words.jpg",
-      sections: getSections("ing22soz/ing22soz", 4),
-      content: "res/books/ing22soz/fihrist.txt"
+      sections: getSections("ingsozler/ingsozler", 73),
+      content: "res/books/ingsozler/fihrist.txt"
+    },
+    {
+      id: 26,
+      title: "Tabiat Risalesi",
+      img: "img/books_cover/nature.jpg",
+      sections: getSections("ingtabiat/ingtabiat", 5),
+      content: "res/books/ingtabiat/fihrist.txt"
+    },
+    {
+      id: 27,
+      title: "Tesettür Risalesi",
+      img: "img/books_cover/ingtesettur.jpg",
+      sections: getSections("ingtesettur/ingtesettur", 2),
+      content: "res/books/ingtesettur/fihrist.txt"
+    },
+    {
+      id: 28,
+      title: "Uhuvvet Risalesi (22.Mektub)",
+      img: "img/books_cover/ikhlas.jpg",
+      sections: getSections("inguhuvvet/inguhuvvet", 2),
+      content: "res/books/inguhuvvet/fihrist.txt"
     }
   ]
 }
