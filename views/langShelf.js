663362f16a168e8a7035c56702f769d6f750a289
Yryskul <mail.iriskul@gmail.com>
2020-10-14 11:19:39 +0300

    langshelf fix

--- views/langShelf.js
+++ views/langShelf.js
@@ -21,18 +21,18 @@ function getLangView() {
     template: "<div class='webix_strong' style='text-align:center;'>#title#</div>" +
       "<img src='#source#' style='margin:auto;display:block;' alt='Girl in a jacket' width='70' height='70'>",
     data: [
-      { id: "en", title: ("English"), source: "img/flags/england.svg" },
-      { id: "arb", title: ("Arabic"), source: "img/flags/saudi-arabia.svg" },
-      { id: "ru", title: ("Russian"), source: "img/flags/russia.svg" },
-      { id: "fr", title: ("French"), source: "img/flags/france.svg" },
-      { id: "gr", title: ("German"), source: "img/flags/german.svg" },
-      { id: "chi", title: ("Chinese"), source: "img/flags/china.svg" },
-      { id: "ind", title: ("Indonesian"), source: "img/flags/indonesia.svg" },
-      { id: "jap", title: ("Japanese"), source: "img/flags/japan.svg" },
-      { id: "pers", title: ("Persian"), source: "img/flags/iran.svg" },
-      { id: "uzb", title: ("Uzbek"), source: "img/flags/uzbekistan.svg" },
-      { id: "spa", title: ("Spanish"), source: "img/flags/spain.svg" },
-      { id: "kor", title: ("Korean"), source: "img/flags/south-korea.svg" },
+      { id: "en", title: _("English"), source: "img/flags/england.svg" },
+      { id: "arb", title: _("Arabic"), source: "img/flags/saudi-arabia.svg" },
+      { id: "ru", title: _("Russian"), source: "img/flags/russia.svg" },
+      { id: "fr", title: _("French"), source: "img/flags/france.svg" },
+      { id: "gr", title: _("German"), source: "img/flags/german.svg" },
+      { id: "chi", title: _("Chinese"), source: "img/flags/china.svg" },
+      { id: "ind", title: _("Indonesian"), source: "img/flags/indonesia.svg" },
+      { id: "jap", title: _("Japanese"), source: "img/flags/japan.svg" },
+      { id: "pers", title: _("Persian"), source: "img/flags/iran.svg" },
+      { id: "uzb", title: _("Uzbek"), source: "img/flags/uzbekistan.svg" },
+      { id: "spa", title: _("Spanish"), source: "img/flags/spain.svg" },
+      { id: "kor", title: _("Korean"), source: "img/flags/south-korea.svg" },
     ]
   }
 }
